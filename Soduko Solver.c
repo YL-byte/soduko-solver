be715e3798223161175ae96e3dbf257f56e319ae
YL-byte <56436958+YL-byte@users.noreply.github.com>
2021-03-03 18:21:11 +0200

    Update Soduko Solver.c

--- Soduko Solver.c
+++ Soduko Solver.c
@@ -5,9 +5,9 @@
 #define N 3
 #define EMPTY_SPACE 0
 
-void solveSoduko(int soduko[N*N][N*N]);
+bool solveSoduko(int soduko[N*N][N*N]);
 
-void aux(int soduko[N*N][N*N], int depth);
+bool aux(int soduko[N*N][N*N], int depth);
 
 void printBoard(int soduko[N*N][N*N]);
 
@@ -15,7 +15,7 @@ bool isValid(int soduko[N*N][N*N]);
 
 int main(){
 
-   int soduko[N*N][N*N] = { //52 empty cells
+   int soduko[N*N][N*N] = {
         {5,3,0,0,7,0,0,0,0},
         {6,0,0,1,9,5,0,0,0},
         {0,9,8,0,0,0,0,6,0},
@@ -27,66 +27,47 @@ int main(){
         {0,0,0,0,8,0,0,7,9}
     };
 
-    /*
-    int soduko[N*N][N*N] = {
-    {4,8,3,9,2,1,6,5,7},
-    {9,6,7,3,4,5,8,2,1},
-    {2,5,0,8,7,6,4,9,3}, //1
-    {5,4,8,1,3,2,9,7,6},
-    {7,2,9,5,6,4,1,3,8},
-    {1,3,6,7,9,8,2,4,5},
-    {3,7,2,6,0,9,5,0,4}, //8, 1
-    {8,1,4,2,5,3,7,6,9},
-    {6,9,0,4,1,7,3,8,0}, //5, 2
-    };
+    bool result = solveSoduko(soduko);
 
-    int soduko[N*N][N*N] = { //Print All Soduko Solutions
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    {0,0,0,0,0,0,0,0,0},
-    };
-    */
-
-    solveSoduko(soduko);
-
-}
-
-void solveSoduko(int soduko[N*N][N*N]){
-
-    aux(soduko, 0);
-
-}
-
-void aux(int soduko[N*N][N*N], int depth){
-
-    //We reached a solution
-    if (depth == N*N*N*N){
-
+    if (result)
         printBoard(soduko);
 
-        return;
+}
 
-    }
+bool solveSoduko(int soduko[N*N][N*N]){
+
+    return aux(soduko, 0);
+
+}
+
+bool aux(int soduko[N*N][N*N], int depth){
+
+    //We reached a solution
+    if (depth == N*N*N*N)
+        return true;
 
     int current_row = depth / (N*N), current_col = depth % (N*N);
+
+    bool result = false;
 
     //No need to try adding a number to a non-empty spot
     if (soduko[current_row][current_col] == EMPTY_SPACE){
 
         for (int number = 1; number <= N*N; number++){
 
+            result = false;
+
             soduko[current_row][current_col] = number;
 
             //This is a valid solution
             if(isValid(soduko))
-                aux(soduko, depth + 1);
+                result = aux(soduko, depth + 1);
 
+            //We found a solution
+            if (result == true)
+                return true;
+
+            //This number in this current location has no solution
             soduko[current_row][current_col] = EMPTY_SPACE;
 
         }
@@ -94,7 +75,9 @@ void aux(int soduko[N*N][N*N], int depth){
     }
 
     else
-        aux(soduko, depth + 1);
+        result = aux(soduko, depth + 1);
+
+    return result;
 
 }
 
